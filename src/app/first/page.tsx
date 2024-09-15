0f6035e942b5e6bd53911773f6f060959a0b0fd3
Frame <90117471+Frame3@users.noreply.github.com>
2024-09-15 17:24:33 -0300

    top

--- src/app/first/page.tsx
+++ src/app/first/page.tsx
@@ -14,7 +14,9 @@ export default function Primeiro() {
     return (
         <div>
             <h1>Primeiro</h1>
-            <Link href={"/"}>Home</Link>
+            <Link href={"/"} className="inline-block px-7 py-2 border-4 text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                Home
+            </Link>
             <div className="flex flex-col">
                 <span>{1 + 1}</span>
                 <span>{Math.random()}</span>
--- src/app/flexbox/page.tsx
+++ src/app/flexbox/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function flexbox() {
     return (
         <div>
-            <Link href={"/"} className="inline-block px-6 py-3 text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+            <Link href={"/"} className="inline-block px-7 py-2 border-4 text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
                 Home
             </Link>
             <h1>Flexbox</h1>
